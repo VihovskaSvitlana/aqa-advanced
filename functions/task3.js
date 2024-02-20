5e02ce720e0d2a17211a9fa344f962714b7cb795
VihovskaSvitlana <svitlana.vihovska@gmail.com>
2024-02-20 12:28:55 +0100

    two ways

--- functions/task3.js
+++ functions/task3.js
@@ -1,3 +1,12 @@
+function checkOrder(available, ordered) {
+    if (available < ordered) return 'Your order is too large, we don’t have enough goods.'
+    if (ordered == 0) return 'Your order is empty.';
+    if (available >= ordered && ordered > 0) return 'Your order is accepted.';
+    return 'Please, enter correct number of goods';
+
+};
+console.log(checkOrder(2, 2));
+
 function checkOrder(available,ordered) {
     if (available < ordered) {
         return 'Your order is too large, we don’t have enough goods.';
