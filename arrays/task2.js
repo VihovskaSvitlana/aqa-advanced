ca3b8717b51ff3f1e58a485e86de192984d5f420
VihovskaSvitlana <svitlana.vihovska@gmail.com>
2024-02-24 19:03:24 +0100

    delete arrays folder

--- arrays/task2.js
+++
@@ -1,8 +0,0 @@
-const numbers = [1, 2, 3, 4, 5, 10];
-const multiplyByIndexArr = numbers.map(multiplyByIndex);
-
-function multiplyByIndex(num) {
-  return num * numbers.indexOf(num);
-};
-
-console.log(multiplyByIndexArr);
