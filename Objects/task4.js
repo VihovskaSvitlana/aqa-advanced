5514ecd67a3540251702dd5b306019c2f4e27c60
VihovskaSvitlana <svitlana.vihovska@gmail.com>
2024-02-24 18:42:22 +0100

    added changes  to HW

--- Objects/task4.js
+++ Objects/task4.js
@@ -1,21 +1,10 @@
-/*
-Завдання 4
-Створіть об'єкт person з такими властивостями:
-firstName: рядок, представляє ім'я особи.
-lastName: рядок, представляє прізвище особи.
-age: число, представляє вік особи.
-Додайте до об'єкта person властивість email, яка буде містити електронну пошту особи.
-Видаліть властивість age з об'єкта person.
-Виведіть оновлений об'єкт person в консоль.
-*/
-
 const person = {
-    firstName : 'Svitlana',
-    lastName : "Vihovska",
-    age : 25
+    firstName: 'Svitlana',
+    lastName: "Vihovska",
+    age: 25
 };
 
 person.email = 'svitlana@gmail.com';
-person
+delete person.age;
 
 console.log(person);
