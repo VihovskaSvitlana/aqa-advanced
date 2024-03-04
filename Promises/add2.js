3f7e689af07696cb82dfaf05a6bc01ce7af04742
VihovskaSvitlana <svitlana.vihovska@gmail.com>
2024-03-04 21:06:28 +0100

    hw of promises

---
+++ Promises/add2.js
@@ -0,0 +1,15 @@
+let checkNumber = (num) => {
+    return new Promise((resolve, reject) => {
+        if (num < 10) {
+            resolve('Success')
+        } else {
+            reject(new Error('Failure'))
+        }
+    })
+}
+
+checkNumber(9)
+    .then(result => console.log(result))
+    .catch((error) =>
+        console.log(error)
+    );
